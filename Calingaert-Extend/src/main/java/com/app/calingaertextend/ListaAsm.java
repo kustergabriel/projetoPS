8035551acf3c9d7b64202c4505aa901ed0d5010f
Gabriel Kuster <gabrielazevedo1812@gmail.com>
2025-07-16 19:11:24 -0300

    parte da luana adicionada

---
+++ Calingaert-Extend/src/main/java/com/app/calingaertextend/ListaAsm.java
@@ -0,0 +1,17 @@
+package com.app.calingaertextend;
+
+public class ListaAsm{
+   private String conteudo;
+   private String tipo;
+
+    ListaAsm (String conteudo, String tipo){
+        this.conteudo = conteudo;
+        this.tipo = tipo;
+    }
+    public String getConteudo(){
+        return conteudo;
+    }
+    public String getTipo (){
+        return tipo;
+    }
+}
